cd6009e8916e166fdf37ee6916f257be08ecc451
habibb2r <habibhk127@gmail.com>
2025-03-25 00:48:37 +0600

    JWT decode to set User

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,24 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../../redux/features/auth/authApi";
+import { useAppDispatch } from "../../redux/hooks";
+import { setUser } from "../../redux/features/auth/authSlice";
 
 
 const Login = () => {
     const {register, handleSubmit} = useForm();
    const [login, {data, error}] = useLoginMutation();
-
+    const dispatch = useAppDispatch()
    console.log("data =>", data)
    console.log("error =>", error)
-    const onSubmit = (data) => {
+    const onSubmit = async(data) => {
         const userInfo = {
             id: '0001',
             password: 'admin12345'
         }
-       login(userInfo)
+       const res = await login(userInfo).unwrap();
+       dispatch(setUser({user: {}, token: res.data.accessToken}))
+       console.log("res =>", res)
     }
     return (
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}} >
