4921a673574318c9c7ce3bee6fc0264e3dc3e2ab
habibb2r <habibhk127@gmail.com>
2025-03-28 23:23:28 +0600

    updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,8 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1', credentials: "include",
+const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1', credentials: 'include',
     prepareHeaders: (headers, { getState}) => {
         const token = (getState() as RootState).auth.token;
         if(token){
@@ -13,23 +13,32 @@ const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1', cred
  });
 
 
- const baseQueryWithRefreshToken = async(args, api, extraOptions) => {
-     const result = await baseQuery(args, api, extraOptions);
-     if(result.error?.status === 401){
+ // eslint-disable-next-line @typescript-eslint/no-explicit-any
+ const baseQueryWithRefreshToken : BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> = async(args, api, extraOptions) : Promise<any> => {
+     let result = await baseQuery(args, api, extraOptions);
+     if(result?.error?.status === 401){
         console.log('Sending Refresh Token')
         const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
             method: 'POST',
             credentials: 'include',
-        })
+        });
         const data = await res.json();
-        const user = (api.getState() as RootState).auth.user;
-        api.dispatch(
-            setUser({
-                user,
-                token: data.accessToken
+        console.log(data)
 
-            })
-        )
+        if(data?.data?.accessToken){
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(
+                setUser({
+                    user,
+                    token: data.data.accessToken
+    
+                })
+            )
+             result = await baseQuery(args, api, extraOptions);
+        }else{
+            api.dispatch(logout())
+        }
+      
      }
      return result;
  }
